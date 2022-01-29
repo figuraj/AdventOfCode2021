6d179c28f6ee8e5af728ae03e2ab1fe1de0ad10b
figura.j <figura.j@pg.com>
2022-01-29 14:32:00 +0100

    Day 15 refactor

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -1,9 +1,7 @@
+import scala.annotation.tailrec
+import scala.io.Source
 
 object Day15 extends App {
-
-  import scala.annotation.tailrec
-  import scala.io.Source
-  import scala.math.{abs, min}
 
   val in = Source.fromFile(getClass.getResource("/inputs/15_input.txt").getFile)
   val inputs = in.getLines().toArray
@@ -14,17 +12,11 @@ object Day15 extends App {
     yield (y, x) -> inputs(x)(y).asDigit).toMap
   }
 
-  case class Node(coordinates: (Int, Int), cost: Int) {
-    def getCostWithHeuristic(end: (Int, Int)): Int = cost + abs(end._1 - coordinates._1) + abs(end._2 - coordinates._2)
-  }
-
-  def getNeighbors(coordinates: (Int, Int), field: Map[(Int, Int), Int], max_x: Int, max_y: Int, expanded: Map[(Int, Int), Int]): Set[(Int, Int)] = {
+  def getNeighbors(coordinates: (Int, Int), max_x: Int, max_y: Int, expanded: Map[(Int, Int), Int]): Set[(Int, Int)] = {
     Set((coordinates._1 + 1, coordinates._2), (coordinates._1 - 1, coordinates._2),
       (coordinates._1, coordinates._2 + 1), (coordinates._1, coordinates._2 - 1))
       .filter(x => (x._1 <= max_x) && x._1 >= 0 && x._2 <= max_y && x._2 >= 0)
       .filterNot(x => expanded.contains(x))
-      //.map(x => Node(x, cost + field(x)))
-    //      .collect { case x if visited(x) >= (cost + field(x)) => Node(x,cost + field(x))}
   }
 
   def solve(field: Map[(Int, Int), Int], start: (Int, Int), end: (Int, Int)): Int = {
@@ -37,14 +29,13 @@ object Day15 extends App {
         expanded(end)
       } else {
         val min_node = prio_queue.minBy(expanded)
-        val next = getNeighbors(min_node, field, x_max, y_max, expanded)
+        val next = getNeighbors(min_node, x_max, y_max, expanded)
         val next_prio = (prio_queue - min_node) ++ next
-        val next_visited = next.foldLeft(expanded)((accum, elem) => accum + (elem -> (accum(min_node) + field(elem))))
-        iter(next_prio, next_visited)
+        val next_expanded = next.foldLeft(expanded)((accum, elem) => accum + (elem -> (accum(min_node) + field(elem))))
+        iter(next_prio, next_expanded)
       }
     }
-
-    iter(Set(start), Map(start -> 0).withDefaultValue(10000000))
+    iter(Set(start), Map(start -> 0))
   }
 
 
